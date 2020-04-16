32f0b64d73804f77e1476bc339d8ea6f18a4c48e
rehan900 <63817771+rehan900@users.noreply.github.com>
2020-04-17 01:37:39 +0500

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,28 +1,18 @@
-/*Do not modify {// [[*... // ]]*} pattern manually*/
+
+
 // [[Page Appearance
 xui.ini.$PageAppearance = {
-//    "theme": "moonify",
-//     "background":{
-//       "background-color": "#FFFFFF",
-//       "background-image": "",
-//       "background-repeat": "",
-//       "background-position": "",
-//       "background-attachment": ""
-//    }
+    "background":{
+        "background-color":"#AFEEEE"
+    },
+    "theme":"default"
 };
 // ]]Page Appearance
-
 // To set Font Icons CDN
-// [[Font Icons CDN
-xui.ini.$FontIconsCDN ={
-// "fontawesome":{
-//     "href":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-//     "integrity":"sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
-//     "crossorigin":"anonymous"
-// }
-};
-// ]]Font Icons CDN
 
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
 // To set default prop to all xui.UI instances
 // [[Default Prop
 xui.ini.$DefaultProp={
@@ -38,3 +28,13 @@ xui.constant = {
     "request_url":"http://www.crossui.com/demo/CRUD/request.php"
 };
 // ]]Constant Data
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf":{
+        "width":800,
+        "height":600
+    }
+};
+// ]]Develop Env Setting
